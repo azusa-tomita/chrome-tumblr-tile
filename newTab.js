95e66e71ecd89bc049a74afb4c141caa8ec5d624
azusa-tomita <azusa.tomita@linecorp.com>
2015-03-10 09:33:56 +0900

    add console

--- newTab.js
+++ newTab.js
@@ -1,3 +1,19 @@
 $(function() {
+  $('input').click(function (E) {
+    E.stopPropagation();
+  });
+  $('.save input').click(function () {
+    location.reload();
+  });
+  $('.close').click(function (E) {
+    $(this).hide();
+    $('.open').show();
+    E.stopPropagation();
+  });
+  $('body').click(function(){
+    $('.open').hide();
+    $('.close').show();
+  });
+
     tumblrTile.draw();
 });
